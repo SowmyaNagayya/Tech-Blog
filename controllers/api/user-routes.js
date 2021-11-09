9137d4a87b58385dc1c1f46403a571c1b2610050
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-09 09:44:00 -0600

    changed in controllers

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const User = require('../../models/User');
 
 //when click on login from nav bar
 router.post('/', async (req, res) => {
